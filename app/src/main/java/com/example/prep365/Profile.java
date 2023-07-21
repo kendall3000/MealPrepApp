72a7e6b9ba99c882a5cf271a729ffe19cac6388d
kdi-0 <kerwin.dahomey@gmail.com>
2023-07-20 19:48:14 -0700

    Add BMI calc and range functionality

--- app/src/main/java/com/example/prep365/Profile.java
+++ app/src/main/java/com/example/prep365/Profile.java
@@ -3,10 +3,21 @@ package com.example.prep365;
 import java.util.ArrayList;
 
 public class Profile {
+    enum BMIClassification {
+        SevereThinness,
+        ModerateThinness,
+        MildThinness,
+        Normal,
+        Overweight,
+        ObeseClassI,
+        ObeseClassII,
+        ObeseClassIII,
+    }
 
     double weight;
     double height;
     double bmi;
+    BMIClassification bmiClass;
     ArrayList<String> allergyList;
     public Profile() {
     }
@@ -14,7 +25,7 @@ public class Profile {
     public Profile(double weight, double height) {
         this.weight = weight;
         this.height = height;
-        this.calculateBMI();
+        calculateBMI();
     }
 
     public double getWeight() {
@@ -42,7 +53,47 @@ public class Profile {
     }
 
     public void calculateBMI() {
-        //TODO
+        this.setBmi((this.weight / Math.pow(this.height, 2))*10000);
+        double roundedBMI = getBmi();
+        if (roundedBMI < 16)
+        {
+            this.bmiClass = BMIClassification.SevereThinness;
+        }
+
+        else if (roundedBMI >= 16 && roundedBMI < 17)
+        {
+            this.bmiClass = BMIClassification.ModerateThinness;
+
+        }
+
+        else if (roundedBMI >= 17 && roundedBMI < 18.5)
+        {
+            this.bmiClass = BMIClassification.MildThinness;
+
+        }
+
+        else if (roundedBMI >= 18.5 && roundedBMI < 25)
+        {
+            this.bmiClass = BMIClassification.Normal;
+        }
+
+        else if (roundedBMI >= 25 && roundedBMI < 30)
+        {
+            this.bmiClass = BMIClassification.Overweight;
+        }
+
+        else if (roundedBMI >= 30 && roundedBMI < 35)
+        {
+            this.bmiClass = BMIClassification.ObeseClassI;
+        }
+
+        else if (roundedBMI >= 35 && roundedBMI < 40)
+        {
+            this.bmiClass = BMIClassification.ObeseClassII;
+        }
+        else {
+            this.bmiClass = BMIClassification.ObeseClassIII;
+        }
     }
 
     public ArrayList<String> getAllergyList() {
--- app/src/main/java/com/example/prep365/UserActivity.java
+++ app/src/main/java/com/example/prep365/UserActivity.java
@@ -1,9 +1,9 @@
 package com.example.prep365;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,14 +14,11 @@ public class UserActivity extends AppCompatActivity {
     private EditText weightField;
     private EditText heightField;
     private TextView bmiField;
-    private Button calculateBMIButton;
     private Button addAllergyButton;
-    private Button removeAllergyButton;
-    private Button viewAllergiesButton;
-    private Button mealPlanButton;
 
     private Profile profile;
 
+     @SuppressLint("DefaultLocale")
      @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,20 +26,19 @@ public class UserActivity extends AppCompatActivity {
 
         weightField = findViewById(R.id.weightInput);
         heightField = findViewById(R.id.heightInput);
-        calculateBMIButton = findViewById(R.id.calculateBMIbutton);
+        Button calculateBMIButton = findViewById(R.id.calculateBMIbutton);
         bmiField = findViewById(R.id.calculatedBMI);
+        TextView bmiClassField = findViewById(R.id.bmiClass);
 
-         calculateBMIButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String w, h;
-                if ((w = String.valueOf(weightField.getText())).isEmpty() || (h = String.valueOf(heightField.getText())).isEmpty()) {
-                    Toast.makeText(UserActivity.this, "Weight or height field empty/invalid", Toast.LENGTH_SHORT).show();
-                } else {
-                    profile = new Profile(Double.parseDouble(w), Double.parseDouble(h));
-                    bmiField.setText("Placeholder");
-                }
-            }
-        });
+         calculateBMIButton.setOnClickListener(view -> {
+             String w, h;
+             if ((w = String.valueOf(weightField.getText())).isEmpty() || (h = String.valueOf(heightField.getText())).isEmpty()) {
+                 Toast.makeText(UserActivity.this, "Weight or height field empty/invalid", Toast.LENGTH_SHORT).show();
+             } else {
+                 profile = new Profile(Double.parseDouble(w), Double.parseDouble(h));
+                 bmiField.setText(String.format("BMI: %.2f", profile.getBmi()));
+                 bmiClassField.setText(String.format("BMI Range: %s", profile.bmiClass.toString()));
+             }
+         });
     }
 }
